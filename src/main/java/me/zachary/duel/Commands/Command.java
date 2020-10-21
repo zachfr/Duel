a9fd4d947d767f328685a48d29b401126348ccaf
Zach_FR <zac232003@icloud.com>
2020-10-21 16:26:30 -0400

    Clean up command

---
+++ src/main/java/me/zachary/duel/Commands/Command.java
@@ -0,0 +1,119 @@
+package me.zachary.duel.Commands;
+
+import me.zachary.duel.Arenas.Arena;
+import me.zachary.duel.Duel;
+import me.zachary.duel.Utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import xyz.theprogramsrc.supercoreapi.spigot.commands.CommandResult;
+import xyz.theprogramsrc.supercoreapi.spigot.commands.SpigotCommand;
+import xyz.theprogramsrc.supercoreapi.spigot.utils.SpigotConsole;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Command extends SpigotCommand {
+    private Duel duel;
+    public Command(Duel duel) {
+        this.duel = duel;
+    }
+
+    @Override
+    public String getCommand() {
+        return "duel";
+    }
+
+    @Override
+    public CommandResult onPlayerExecute(Player player, String[] strings) {
+        String targetName = strings[0];
+        if (strings.length == 0 || strings[0].equalsIgnoreCase("help")) {
+            for(String message : duel.getConfig().getStringList("HelpCommand")){
+                player.sendMessage(Utils.chat(message));
+            }
+        }else if (strings[0].equalsIgnoreCase("accept")) {
+            if (duel.players.containsKey(player)){
+                player.sendMessage(Utils.chat(duel.getConfig().getString("Start_Duel")));
+
+                Player firstPlayerTarget = duel.players.get(player);
+                firstPlayerTarget.sendMessage(Utils.chat(duel.getConfig().getString("Start_Duel")));
+
+                duel.arenaManager.joinArena(player, firstPlayerTarget);
+
+                duel.players.remove(player);
+
+            }
+        }else if (strings[0].equalsIgnoreCase("deny")) {
+            if (duel.players.containsKey(player)){
+                player.sendMessage(Utils.chat(duel.getConfig().getString("Deny_Duel")));
+
+                Player firstPlayerTarget = duel.players.get(player);
+                firstPlayerTarget.sendMessage(Utils.chat(duel.getConfig().getString("Player_Deny_Duel").replace("<PlayerDenyDuel>", player.getName())));
+
+                duel.players.remove(player);
+            }
+        }else if(strings[0].equalsIgnoreCase("createarena")) {
+            if (player.hasPermission("duel.createarena")) {
+                if (strings.length < 4) {
+                    player.sendMessage("/duel createarena <loc1> <loc2> <ArenaName>");
+                    return CommandResult.COMPLETED;
+                }
+
+                Location loc1 = parseStringToLoc(strings[1]);
+                Location loc2 = parseStringToLoc(strings[2]);
+                Arena arena = new Arena(loc1, loc2);
+                String arenaName = "arena-" + strings[3];
+
+                duel.arenaConfig.set("arenas." + arenaName + ".loc1", strings[1]);
+                duel.arenaConfig.set("arenas." + arenaName + ".loc2", strings[2]);
+
+                duel.saveArenaConfig();
+                duel.arenaManager.addArena(arena);
+
+                player.sendMessage(Utils.chat(duel.getConfig().getString("Create_Arena").replace("<ArenaName>", arenaName)));
+            }
+        }else if (Bukkit.getPlayer(targetName) != null) {
+            Player target = Bukkit.getPlayer(targetName);
+
+            if (player == target) {
+                player.sendMessage(Utils.chat(duel.getConfig().getString("ask_yourself")));
+                return CommandResult.COMPLETED;
+            }
+
+            if (duel.players.containsKey(target)){
+                player.sendMessage(Utils.chat(duel.getConfig().getString("Player_Already_Have_Requests")));
+                return CommandResult.COMPLETED;
+            }
+
+            duel.players.put(target, player);
+            player.sendMessage(Utils.chat(duel.getConfig().getString("Ask_Player").replace("<player>", targetName)));
+            target.sendMessage(Utils.chat(duel.getConfig().getString("Receive_Duel").replace("<player>", player.getName())));
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + target.getName() + " [\"\",{\"text\":\"Click here to [ACCEPT]\",\"color\":\"green\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/duel accept\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":[\"\",{\"text\":\"Click to accept duel\"}]}}]");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + target.getName() + " [\"\",{\"text\":\"Click here to [DENY]\",\"color\":\"red\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/duel deny\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":[\"\",{\"text\":\"Click to deny duel\"}]}}]");
+        }else
+            return CommandResult.COMPLETED;
+        return CommandResult.COMPLETED;
+    }
+
+    @Override
+    public CommandResult onConsoleExecute(SpigotConsole spigotConsole, String[] strings) {
+        return CommandResult.COMPLETED;
+    }
+
+    public Location parseStringToLoc(String string) {
+        String[] parsedLoc = string.split(",");
+        double x = Double.valueOf(parsedLoc[0]);
+        double y = Double.valueOf(parsedLoc[1]);
+        double z = Double.valueOf(parsedLoc[2]);
+
+        return new Location(Bukkit.getWorld("world"), x,y,z);
+    }
+
+    @Override
+    public List<String> getCommandComplete(Player player, String alias, String[] args) {
+        List<String> help = new ArrayList<>();
+        help.add("help");
+        if(player.hasPermission("duel.createarena")) help.add("createarena");
+        return help;
+    }
+}
--- src/main/java/me/zachary/duel/Duel.java
+++ src/main/java/me/zachary/duel/Duel.java
@@ -3,41 +3,42 @@ package me.zachary.duel;
 import me.zachary.duel.Arenas.Arena;
 import me.zachary.duel.Arenas.ArenaListeners;
 import me.zachary.duel.Arenas.ArenaManager;
+import me.zachary.duel.Commands.Command;
 import me.zachary.duel.Utils.Metrics;
 import me.zachary.duel.Utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 import sun.applet.Main;
+import xyz.theprogramsrc.supercoreapi.spigot.SpigotPlugin;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class Duel extends JavaPlugin {
+public final class Duel extends SpigotPlugin {
 
-    private Map<Player, Player> players = new HashMap<>();
-    private ArenaManager arenaManager = new ArenaManager(this);
+    public Map<Player, Player> players = new HashMap<>();
+    public ArenaManager arenaManager = new ArenaManager(this);
     private File arenaFile;
-    private YamlConfiguration arenaConfig;
+    public YamlConfiguration arenaConfig;
     private Main plugin;
 
     public Duel() {
     }
 
     @Override
-    public void onEnable() {
+    public void onPluginEnable() {
         int pluginId = 9146;
         Metrics metrics = new Metrics(this, pluginId);
 
         getCommand("duel").setExecutor(this);
         getServer().getPluginManager().registerEvents(new ArenaListeners(this), this);
+        new Command(this);
 
         loadArenaConfig();
         saveDefaultConfig();
@@ -58,7 +59,7 @@ public final class Duel extends JavaPlugin {
 
     }
 
-    private void loadArenaConfig() {
+    public void loadArenaConfig() {
         if (!getDataFolder().exists()){
             getDataFolder().mkdir();
         }
@@ -76,108 +77,7 @@ public final class Duel extends JavaPlugin {
         arenaConfig = YamlConfiguration.loadConfiguration(arenaFile);
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (label.equalsIgnoreCase("duel") && sender instanceof Player){
-            Player player = (Player) sender;
-
-            if (args.length == 0) {
-                for(String message : this.getConfig().getStringList("HelpCommand")){
-                    sender.sendMessage(Utils.chat(message));
-                }
-                return true;
-            }
-            if (args.length >= 1){
-                String targetName = args[0];
-
-                if (args[0].equalsIgnoreCase("accept")){
-
-                    if (players.containsKey(player)){
-                        player.sendMessage(Utils.chat(this.getConfig().getString("Start_Duel")));
-
-                        Player firstPlayerTarget = players.get(player);
-                        firstPlayerTarget.sendMessage(Utils.chat(this.getConfig().getString("Start_Duel")));
-
-                        arenaManager.joinArena(player, firstPlayerTarget);
-
-                        players.remove(player);
-
-                    }
-
-
-                }else if (args[0].equalsIgnoreCase("deny")){
-
-                    if (players.containsKey(player)){
-                        player.sendMessage(Utils.chat(this.getConfig().getString("Deny_Duel")));
-
-                        Player firstPlayerTarget = players.get(player);
-                        firstPlayerTarget.sendMessage(Utils.chat(this.getConfig().getString("Player_Deny_Duel").replace("<PlayerDenyDuel>", player.getName())));
-
-                        players.remove(player);
-                    }
-
-                } else if (args[0].equalsIgnoreCase("createarena")) {
-                    if (player.hasPermission("duel.createarena")) {
-                        if (args.length < 4) {
-                            player.sendMessage("/duel createarena <loc1> <loc2> <ArenaName>");
-                            return true;
-                        }
-
-                        Location loc1 = parseStringToLoc(args[1]);
-                        Location loc2 = parseStringToLoc(args[2]);
-                        Arena arena = new Arena(loc1, loc2);
-                        String arenaName = "arena-" + args[3];
-
-                        arenaConfig.set("arenas." + arenaName + ".loc1", args[1]);
-                        arenaConfig.set("arenas." + arenaName + ".loc2", args[2]);
-
-                        saveArenaConfig();
-                        arenaManager.addArena(arena);
-
-                        player.sendMessage(Utils.chat(this.getConfig().getString("Create_Arena").replace("<ArenaName>", arenaName)));
-                    }else {
-                        player.sendMessage(Utils.chat(this.getConfig().getString("No_Permission")));
-                    }
-
-
-                } /*else if (args[0].equalsIgnoreCase("arenalist")) {
-                    if (player.hasPermission("duel.arenalist")) {
-                        player.sendMessage("yea hi");
-                    }else {
-                        player.sendMessage(this.getConfig().getString("No_Permission"));
-                    }
-                }*/ else if (Bukkit.getPlayer(targetName) != null){
-                    Player target = Bukkit.getPlayer(targetName);
-
-                    if (player == target) {
-                        player.sendMessage(Utils.chat(this.getConfig().getString("ask_yourself")));
-                        return true;
-                    }
-
-                    if (players.containsKey(target)){
-                        player.sendMessage(Utils.chat(this.getConfig().getString("Player_Already_Have_Requests")));
-                        return true;
-                    }
-
-                    players.put(target, player);
-                    player.sendMessage(Utils.chat(this.getConfig().getString("Ask_Player").replace("<player>", targetName)));
-                    target.sendMessage(Utils.chat(this.getConfig().getString("Receive_Duel").replace("<player>", player.getName())));
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + target.getName() + " [\"\",{\"text\":\"Click here to [ACCEPT]\",\"color\":\"green\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/duel accept\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":[\"\",{\"text\":\"Click to accept duel\"}]}}]");
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + target.getName() + " [\"\",{\"text\":\"Click here to [DENY]\",\"color\":\"red\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/duel deny\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":[\"\",{\"text\":\"Click to deny duel\"}]}}]");
-
-                }else {
-                    player.sendMessage(Utils.chat(this.getConfig().getString("Player_Not_Connected").replace("<PlayerNotConnected>", targetName)));
-                }
-
-                return true;
-            }
-        }
-        return false;
-        //return super.onCommand(sender, command, label, args);
-    }
-
-    private void saveArenaConfig() {
+    public void saveArenaConfig() {
         try {
             arenaConfig.save(arenaFile);
         } catch (IOException e) {
@@ -203,7 +103,14 @@ public final class Duel extends JavaPlugin {
     }
 
     @Override
-    public void onDisable() {
+    public void onPluginDisable() {
         // Plugin shutdown logic
     }
+
+    @Override
+    public void onPluginLoad() {
+
+    }
+
+
 }
